03bae444f1deb2e3247a2571493fa6d4ac07e447
Librazy <librazy@foxmail.com>
2016-09-22 15:59:55 +0800

    generics version

--- Microsoft.Toolkit.Uwp/Helpers/BackgroundTaskHelper.cs
+++ Microsoft.Toolkit.Uwp/Helpers/BackgroundTaskHelper.cs
@@ -26,22 +26,22 @@ namespace Microsoft.Toolkit.Uwp.Helpers
         /// <summary>
         /// Check if a background task has been registered with the application or not.
         /// </summary>
-        /// <param name="backgroundTaskType">The type of the background task.</param>
+        /// <typeparam name="TBackgroundTaskType">The type of the background task.</typeparam>
         /// <returns>True/False indicating if a background task was registered or not</returns>
-        public static bool IsBackgroundTaskRegistered(Type backgroundTaskType)
+        public static bool IsBackgroundTaskRegistered<TBackgroundTaskType>()
         {
-            return BackgroundTaskRegistration.AllTasks.Any(t => t.Value.Name == backgroundTaskType.Name);
+            return BackgroundTaskRegistration.AllTasks.Any(t => t.Value.Name == typeof(TBackgroundTaskType).FullName);
         }
 
         /// <summary>
         /// Register a background task for the application with conditions
         /// </summary>
-        /// <param name="backgroundTaskType">The type of the background task</param>
+        /// <typeparam name="TBackgroundTaskType">The type of the background task</typeparam>
         /// <param name="trigger">Trigger that indicate when the background task should be invoked</param>
         /// <param name="enforceConditions">Indicate if the background task should quit if condition is no longer valid</param>
         /// <param name="conditions">Optional conditions for the background task to run with</param>
         /// <returns>Background Task that was registered with the system</returns>
-        public static async Task<BackgroundTaskRegistration> RegisterAsync(Type backgroundTaskType, IBackgroundTrigger trigger, bool enforceConditions = true, params IBackgroundCondition[] conditions)
+        public static async Task<BackgroundTaskRegistration> RegisterAsync<TBackgroundTaskType>(IBackgroundTrigger trigger, bool enforceConditions = true, params IBackgroundCondition[] conditions)
         {
             // Verify access
             BackgroundAccessStatus taskRequired = await BackgroundExecutionManager.RequestAccessAsync();
@@ -52,22 +52,15 @@ namespace Microsoft.Toolkit.Uwp.Helpers
             }
 
             // Get details about the background task
-            string backgroundTaskName = backgroundTaskType.Name;
-            string backgroundTaskEntryPoint = backgroundTaskType.FullName;
+            string backgroundTaskEntryPoint = typeof(TBackgroundTaskType).FullName;
 
             // build the background task
             BackgroundTaskBuilder builder = new BackgroundTaskBuilder();
-            builder.Name = backgroundTaskName;
+            builder.Name = backgroundTaskEntryPoint;
             builder.TaskEntryPoint = backgroundTaskEntryPoint;
             builder.CancelOnConditionLoss = enforceConditions;
 
-            if (conditions != null && conditions.Count() > 0)
-            {
-                foreach (IBackgroundCondition condition in conditions)
-                {
-                    builder.AddCondition(condition);
-                }
-            }
+            conditions?.ToList().ForEach(builder.AddCondition);
 
             builder.SetTrigger(trigger);
 
@@ -80,27 +73,24 @@ namespace Microsoft.Toolkit.Uwp.Helpers
         /// <summary>
         /// Unregister a background task
         /// </summary>
-        /// <param name="backgroundTaskType">The type of the background task</param>
-        public static void Unregister(Type backgroundTaskType)
+        /// <typeparam name="TBackgroundTaskType">The type of the background task</typeparam>
+        public static void Unregister<TBackgroundTaskType>()
         {
-            if (IsBackgroundTaskRegistered(backgroundTaskType) == false)
+            if (IsBackgroundTaskRegistered<TBackgroundTaskType>() == false)
             {
                 return;
             }
 
-            IBackgroundTaskRegistration tobeUnregister = BackgroundTaskRegistration.AllTasks.Where(t => t.Value.Name == backgroundTaskType.Name).Select(t => t).FirstOrDefault().Value;
+            IBackgroundTaskRegistration toBeUnregistered = BackgroundTaskRegistration.AllTasks.Where(t => t.Value.Name == typeof(TBackgroundTaskType).FullName).Select(t => t).FirstOrDefault().Value;
 
-            tobeUnregister?.Unregister(true);
+            toBeUnregistered?.Unregister(true);
         }
 
         /// <summary>
         /// Get the registered background task of the given type.
         /// </summary>
-        /// <param name="backgroundTaskType">Type of the background task class</param>
+        /// <typeparam name="TBackgroundTaskType">Type of the background task class</typeparam>
         /// <returns>Background task if there is such background task registered. Otherwise, null</returns>
-        public static IBackgroundTaskRegistration GetBackgroundTask(Type backgroundTaskType)
-        {
-            return BackgroundTaskRegistration.AllTasks.FirstOrDefault(t => t.Value.Name == backgroundTaskType.Name).Value;
-        }
+        public static IBackgroundTaskRegistration GetBackgroundTask<TBackgroundTaskType>() => BackgroundTaskRegistration.AllTasks.FirstOrDefault(t => t.Value.Name == typeof(TBackgroundTaskType).FullName).Value;
     }
 }
